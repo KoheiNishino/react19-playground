e5da355955b7a12eb78f703233d2924f458e8ae1
Kohei Nishino <koheinishino.95@gmail.com>
2025-04-16 12:05:26 +0900

    lint

--- src/app/useOptimistic/page.tsx
+++ src/app/useOptimistic/page.tsx
@@ -30,17 +30,17 @@ export default function Page() {
     <form action={dispatch} className="p-4">
       <SubmitButton />
       {optimisticUser !== null
-      && (
-        <div className="mt-4 w-80 rounded-lg border bg-white p-6 shadow-lg">
-          <h2 className="mb-4 text-2xl font-bold text-gray-800">
-            {`${optimisticUser.firstName} ${optimisticUser.lastName}`}
-          </h2>
-          <p className="text-lg text-gray-600">
-            年齢:
-            {optimisticUser.age}
-          </p>
-        </div>
-      )}
+        && (
+          <div className="mt-4 w-80 rounded-lg border bg-white p-6 shadow-lg">
+            <h2 className="mb-4 text-2xl font-bold text-gray-800">
+              {`${optimisticUser.firstName} ${optimisticUser.lastName}`}
+            </h2>
+            <p className="text-lg text-gray-600">
+              年齢:
+              {optimisticUser.age}
+            </p>
+          </div>
+        )}
     </form>
   )
 }
